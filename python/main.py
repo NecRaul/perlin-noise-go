6e7a2e8bad95ab2cb460438a5e95f2ced82fbc6e
NecRaul <necraul2001@gmail.com>
2023-11-15 23:46:13 +0400

    New argument (mean/plot)
    Depending on which one you pass through
    It will either calculate the mean average
    Or show you the plot of all results
    
    I haven't added plot functionality yet

--- python/main.py
+++ python/main.py
@@ -1,19 +1,43 @@
 import sys
 import os
+import numpy
 import shared
 
 
 def main():
-    if len(sys.argv) != 2:
-        print("Usage: python main.py directory_path")
+    if len(sys.argv) != 3:
+        print("Usage: python main.py directory_path mean/plot")
     else:
         directory_path = sys.argv[1]
         if not os.path.isdir(directory_path):
             print("Invalid directory path.")
             return
+        argument = sys.argv[2]
         results = shared.read_directory(directory_path)
-        for result in results:
-            print(f"Mean average in seconds for size {result.name}: {result.average}")
+
+        if argument.lower() == "mean":
+            for result in results:
+                print(
+                    f"Mean average in seconds for size {result.name}: {result.average}"
+                )
+        elif argument.lower() == "plot":
+            max_length = max(len(result.seconds_array) for result in results)
+
+            padded_arrays = [
+                numpy.pad(
+                    result.seconds_array,
+                    (0, max_length - len(result.seconds_array)),
+                    "constant",
+                    constant_values=numpy.nan,
+                )
+                for result in results
+            ]
+
+            combined_array = numpy.vstack(padded_arrays)
+
+            # TODO Add matploblib functionality
+        else:
+            print("Incorrect argument! Argument should either be mean or plot!")
 
 
 if __name__ == "__main__":

61e00631c4d03955d2262efdedaef97d5831d75c
NecRaul <necraul2001@gmail.com>
2023-11-15 23:37:19 +0400

    Changed how average is calculated, added rounding
    Now average is calculated via numpy.mean()
    and rounded to 9th decimal point

--- python/result.py
+++ python/result.py
@@ -1,7 +1,10 @@
+import numpy
+
+
 class Result:
     def __init__(self, name, seconds, count, seconds_array):
         self.name = name.split("/")[-1].split(".")[0]
         self.seconds = seconds
         self.count = count
-        self.average = seconds / count
         self.seconds_array = seconds_array
+        self.average = round(numpy.mean(seconds_array), 9)
